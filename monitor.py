b7c4542a8ff5bcc44ef6cf61fd6dc8f5f6f11f8a
Yarramaddu Manogna (MS/ESC62-VM) <Manogna.Yarramaddu@in.bosch.com>
2025-07-28 19:41:50 +0530

    reduced cyclomatic complexity

--- monitor.py
+++ monitor.py
@@ -1,37 +1,26 @@
-
 from time import sleep
 import sys
 
+def blink_alert(times=6, delay=1):
+    """Display a blinking asterisk animation."""
+    for _ in range(times):
+        print('\r* ', end='')
+        sys.stdout.flush()
+        sleep(delay)
+        print('\r *', end='')
+        sys.stdout.flush()
+        sleep(delay)
 
 def vitals_ok(temperature, pulseRate, spo2):
-  if temperature > 102 or temperature < 95:
-    print('Temperature critical!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  elif pulseRate < 60 or pulseRate > 100:
-    print('Pulse Rate is out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  elif spo2 < 90:
-    print('Oxygen Saturation out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  return True
+    checks = [
+        (temperature > 102 or temperature < 95, 'Temperature critical!'),
+        (pulseRate < 60 or pulseRate > 100, 'Pulse Rate is out of range!'),
+        (spo2 < 90, 'Oxygen Saturation out of range!')
+    ]
+    
+    for condition, message in checks:
+        if condition:
+            print(message)
+            blink_alert()
+            return False
+    return True
